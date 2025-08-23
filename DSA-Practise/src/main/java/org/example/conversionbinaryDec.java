287acd8ec8ddcd93b9fd9b2f93e6d743e08d96ee
prateekshankar <pssaxena@gmail.com>
2025-08-23 23:26:26 +0530

    First commit GCD * LCM

---
+++ DSA-Practise/src/main/java/org/example/conversionbinaryDec.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.time.Clock;
+
+public class conversionbinaryDec {
+
+
+    public static void main(String[] args) {
+        System.out.println("binary conversion"+ decimal("1001111111"));
+    }
+    public static long decimal(String binary){
+
+        int decimal = 0;
+        for(int i=0;i<binary.length();i++){
+
+           decimal=(decimal<<1)+ (binary.charAt(i)-'0');
+        }
+     return decimal;
+    }
+}
